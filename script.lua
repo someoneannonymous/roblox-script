cf59b2ac03d9150889e32445b1d4fdc33c34b808
someone <nazarmeleshkopro@gmail.com>
2026-06-14 16:57:20 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,17 +1,26 @@
 local function apply(part)
-	part.Transparency = 0
-	part.Color = Color3.fromRGB(0, 255, 0)
+	if part.Name == "glass_panel_weak" then
+		part.Transparency = 0
+		part.Color = Color3.fromRGB(255, 0, 0) -- Red
+	elseif part.Name == "glass_panel" then
+		part.Transparency = 0
+		part.Color = Color3.fromRGB(0, 255, 0) -- Green
+	end
 end
 
 workspace.DescendantAdded:Connect(function(obj)
-	if obj:IsA("BasePart") and obj.Name == "glass_panel" then
-		apply(obj)
+	if obj:IsA("BasePart") then
+		if obj.Name == "glass_panel" or obj.Name == "glass_panel_weak" then
+			apply(obj)
+		end
 	end
 end)
 
--- optional initial scan once
+-- initial scan once
 for _, obj in ipairs(workspace:GetDescendants()) do
-	if obj:IsA("BasePart") and obj.Name == "glass_panel" then
-		apply(obj)
+	if obj:IsA("BasePart") then
+		if obj.Name == "glass_panel" or obj.Name == "glass_panel_weak" then
+			apply(obj)
+		end
 	end
 end
